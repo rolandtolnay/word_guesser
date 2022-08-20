1f871d5dc7773d2b4ccd20641f2c4a0511efc5f5
Roland Tolnay <roland@mindsystemsolutions.com>
2022-08-20 23:16:09 +0300

    Implemented hints

--- lib/domain/model/word_model.dart
+++ lib/domain/model/word_model.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
-import 'package:injectable/injectable.dart';
 
 import 'firestore_serializable_annotation.dart';
 
